868a446086ce3fa0a2fe4cd124587aea0bc0840d
Gavin Coffelt <gavincoffelt@yahoo.com>
2019-03-08 15:17:29 -0800

    First Commit?

---
+++ Assets/Scripts/Vehicle/UIButtons.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIButtons : MonoBehaviour
+{
+    GameObject Player;
+    VehiclePhysics PlayerPhysics;
+
+    public GameObject Stick;
+
+    bool GasPressed;
+    bool ReversePressed;
+    int Speed = 1;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PlayerPhysics = Player.GetComponent<VehiclePhysics>();
+        Stick = GameObject.FindGameObjectWithTag("JoyStick");
+    }
+
+    void Update()
+    {
+        if (GasPressed || ReversePressed)
+        {
+            PlayerPhysics.MovementDirection += Speed * Time.deltaTime;
+        }
+        else if(PlayerPhysics.MovementDirection != 0)
+        {
+            PlayerPhysics.MovementDirection -= Mathf.Lerp(PlayerPhysics.MovementDirection, 0, 0.01f) * Time.deltaTime;
+            if (Mathf.Abs(PlayerPhysics.MovementDirection) < 0.1f)
+            {
+                PlayerPhysics.MovementDirection = 0;
+            }
+        }
+    }
+
+    public void GasButton()
+    {
+        GasPressed = true;
+        Speed = 1;
+    }
+    public void ReverseButton()
+    {
+        ReversePressed = true;
+        Speed = -1;
+    }
+    public void NonGasButton()
+    {
+        GasPressed = false;
+    }
+    public void NonReverseButton()
+    {
+        ReversePressed = false;
+    }
+
+
+    //JoyStick
+    public void MoveJoystick(BaseEventData e)
+    {
+        PointerEventData Data = e as PointerEventData;
+        Stick.transform.position = Data.position;
+        PlayerPhysics.MovementTurn = Stick.transform.localPosition.x - 50;
+    }
+    public void ResetJoystick(BaseEventData e)
+    {
+        PointerEventData Data = e as PointerEventData;
+        PlayerPhysics.MovementTurn = 0;
+        Stick.transform.localPosition = Vector3.zero;
+    }
+
+}
---
+++ Assets/Scripts/Vehicle/VehiclePhysics.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiclePhysics : MonoBehaviour
+{
+    //Car Movement
+    float Direction = 0;
+    public float MovementDirection
+    {
+        set
+        {
+            Direction = Mathf.Clamp(value, -1, 1);
+        }
+        get
+        {
+            return Direction;
+        }
+    }
+
+    public float Turn = 0;
+    public float MovementTurn
+    {
+        set
+        {
+            Turn = Mathf.Clamp(value, -100, 100);
+        }
+        get
+        {
+            return Turn;
+        }
+    }
+
+    public float Speed;
+    //Car Physics
+    public float HoverAmount;
+    RaycastHit hit;
+
+    Vector3 ForwardVector;
+    public bool Grounded;
+	void Start ()
+    {
+        Grounded = false;
+	}
+	
+	void Update ()
+    {
+        ApplyGravity();
+        Grounded = CheckGrounded();
+        MoveVehicle();
+        DebugRays();
+	}
+
+    void ApplyGravity()
+    {
+        if(!Grounded)
+        {
+            transform.position += Physics.gravity * Time.deltaTime;
+        }
+    }
+    bool CheckGrounded()
+    {
+        if(Physics.Raycast(transform.position, -Vector3.up, out hit, HoverAmount))
+        {
+            if (hit.transform.gameObject.tag.CompareTo("Ground") == 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    Vector3 GetForwardVector()
+    {
+        if(!Grounded)
+        {
+            return transform.forward;
+        }
+        return (Vector3.Cross(hit.normal, -transform.right));
+    }
+
+    void MoveVehicle()
+    {
+        if (Grounded)
+        {
+            transform.position += GetForwardVector() * Direction * Time.deltaTime * Speed;
+            if(Direction != 0)
+            {
+                transform.Rotate(new Vector3(0, MovementTurn * Time.deltaTime * Mathf.Abs(Direction), 0));
+               // transform.rotation = new Quaternion(0, transform.rotation.y, 0, 0);
+            }
+        }
+    }
+
+    void DebugRays()
+    {
+        Debug.DrawRay(transform.position, GetForwardVector() * 2, Color.blue);
+        //Debug.DrawRay(transform.position, Vector3.forward * 2, Color.green);
+    }
+}
