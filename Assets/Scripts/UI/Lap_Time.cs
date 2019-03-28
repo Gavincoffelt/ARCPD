32c211aefbaa12cd8ee233c1d517d4f8756f4a45
Gavin Coffelt <gavincoffelt@yahoo.com>
2019-03-28 10:56:56 -0700

    Gyro Controls

--- Assets/Scripts/UI/Lap_Time.cs
+++ Assets/Scripts/UI/Lap_Time.cs
@@ -6,18 +6,20 @@ using UnityEngine.UI;
 public class Lap_Time : MonoBehaviour {
     public Text laptext;
     public Text time;
+
     int curlap = 1;
     int totallap = 5;
 
     float jimmy = 0;
     void Start () {
-		
+        time.text = "0";
+        laptext.text = "0";
 	}
 	
 	void Update () {
         jimmy += 1 * Time.deltaTime;
 
         time.text = jimmy.ToString("F3");
-        laptext.text = curlap.ToString() + "/" + totallap.ToString();
+        laptext.text = (curlap.ToString() + "/" + totallap.ToString());
     }
 }
