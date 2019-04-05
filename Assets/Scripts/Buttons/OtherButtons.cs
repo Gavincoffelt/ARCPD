89cbef865005fe46a8694671293a71c5709784ca
Ryan Healy <ryan.healy11@gmail.com>
2019-04-05 10:42:15 -0700

    more conecting of scenes

--- Assets/Scripts/Buttons/OtherButtons.cs
+++ Assets/Scripts/Buttons/OtherButtons.cs
@@ -17,16 +17,26 @@ public class OtherButtons : MonoBehaviour {
 
     public void Next()
     {
+
         SceneManager.LoadScene("VehicleSelection", LoadSceneMode.Single);
     }
 
     public void Back()
     {
+        ButtonManager.Instance = null;
+        Destroy(GameObject.Find("Butooon"));
         SceneManager.LoadScene("TestMenu", LoadSceneMode.Single);
     }
 
     public void Select()
     {
-        SceneManager.LoadScene(butt.MapName, LoadSceneMode.Single);
+        if (butt.ARTog)
+        {
+            SceneManager.LoadScene("ARcore rewrite", LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene(butt.MapName, LoadSceneMode.Single);
+        }
     }
 }
