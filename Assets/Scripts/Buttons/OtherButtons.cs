e339f41ccb4f1e4cf26f11135b7dbce92973b123
Ryan Healy <ryan.healy11@gmail.com>
2019-04-05 11:31:50 -0700

    cleaning

--- Assets/Scripts/Buttons/OtherButtons.cs
+++ Assets/Scripts/Buttons/OtherButtons.cs
@@ -10,11 +10,6 @@ public class OtherButtons : MonoBehaviour {
         butt = GameObject.Find("Butooon").GetComponent<ButtonManager>();
     }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void Next()
     {
 
